37ae6054bdc2d066b66bfb1e22b9bdd91258f6bd
andrewtholt <andrewtholt60@gmail.com>
2013-05-20 08:41:44 +0100

    Small changes

--- RediSQL.c
+++ RediSQL.c
@@ -6,17 +6,50 @@
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
+#include <signal.h>
 
+#include "config.h"
+
+#define MAXLINE 79
 /*
  * Connect to a shared memory segment that is created in main.
  * Needs to be protected by a semaphore.
-*/
+ */
+
+void catch(int snum) {
+    int pid;
+    int status;
+
+    pid=wait(&status);
+
+    printf("Child exited with status\n", status);
+}
 
 int readline( int fd, char *ptr, int maxLen ) {
-
+    int n;
     int cnt = 0;
+    int runFlag = 1;
 
-    n = read(fd,ptr,1);
+    do {
+        n = read(fd,&ptr[cnt],1);
+
+        if( n <= 0) {
+            exit(1);
+        }
+
+        if ( '\n' == ptr[cnt] ) {
+            runFlag=0;
+            ptr[cnt]= '\0';
+        } else {
+            cnt++;
+        }
+
+        if (cnt > maxLen ) {
+            runFlag = 0;
+        }
+    }
+    while( runFlag != 0);
+    return(cnt);
 }
 
 
@@ -34,8 +67,10 @@ void doprocessing (int sock) {
     while( runFlag) {
         bzero(inBuffer,256);
 
-        n = read(sock,inBuffer,255);
-        printf("Read returned %d\n", n);
+//        n = read(sock,inBuffer,255);
+//        printf("Readline.\n");
+        n = readline(sock,inBuffer,255);
+        printf("Readline returned %d\n", n);
 
         if (n < 0) {
             perror("ERROR reading from socket");
@@ -90,7 +125,10 @@ int main( int argc, char *argv[] ) {
     }
     /* Initialize socket structure */
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = 5001;
+    portno = atoi(argv[1]);
+
+    printf("Port=%d\n", portno);
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
@@ -107,6 +145,7 @@ int main( int argc, char *argv[] ) {
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
 
+    signal(SIGCHLD,catch);
     while (1) {
         newsockfd = accept(sockfd, 
                 (struct sockaddr *) &cli_addr, &clilen);
--- config.h
+++ config.h
@@ -1,44 +1,46 @@
-struc config {
+
+struct config {
     int     daemonize;      // 1=yes
     char   *pidfile;        // /var/run/redis.pid
     int     port;           // 6379
     int     timeout;        // 0
-    int tcp-keepalive;      // 0
-    char *loglevel;         // notice
-    int syslog-enabled;     // 0=no
-    char *syslog-ident;     // redis
-    char *syslog-facility;  // local0
-    inst databases;         // 16
-//    save 900 1
-//    save 300 10
-//    save 60 10000
-    int stop-writes-on-bgsave-error; // 1=yes
+    int tcp_keepalive;      // 0
+    char loglevel[32];         // notice
+    int syslog_enabled;     // 0=no
+    char syslog_ident[32];     // redis
+    char *syslog_facility;  // local0
+    int databases;         // 16
+    //    save 900 1
+    //    save 300 10
+    //    save 60 10000
+    int stop_writes_on_bgsave_error; // 1=yes
     int rdbcompression;             //  1=yes
     int rdbchecksum;                //  yes
-    char *dbfilename;               // dump.rdb
-    char *dir                       // /var/redis_6379
-    int slave-serve-stale-data;     // 1=yes
-    int slave-read-only;            // 1=yes
-    int repl-disable-tcp-nodelay    // no
-    slave-priority 100
-    appendonly no
-    appendfsync everysec
-    no-appendfsync-on-rewrite no
-    auto-aof-rewrite-percentage 100
-    auto-aof-rewrite-min-size 64mb
-    lua-time-limit 5000
-    slowlog-log-slower-than 10000
-    slowlog-max-len 128
-    hash-max-ziplist-entries 512
-    hash-max-ziplist-value 64
-    list-max-ziplist-entries 512
-    list-max-ziplist-value 64
-    set-max-intset-entries 512
-    zset-max-ziplist-entries 128
-    zset-max-ziplist-value 64
-    activerehashing yes
-    client-output-buffer-limit normal 0 0 0
-    client-output-buffer-limit slave 256mb 64mb 60
-    client-output-buffer-limit pubsub 32mb 8mb 60
-    hz 10
-}
+    char dbfilename[32];               // dump.rdb
+    char dir[255];                       // /var/redis_6379
+    int slave_serve_stale_data;     // 1=yes
+    int slave_read_only;            // 1=yes
+    int repl_disable_tcp_nodelay;    // no
+    int slave_priority;             // 100
+    int appendonly;              //  no
+    char appendfsync[32];       // everysec
+    int no_appendfsync_on_rewrite;  // no
+    int auto_aof_rewrite_percentage;    // 100
+    int auto_aof_rewrite_min_size;  //  64mb
+    //        lua_time_limit 5000
+    int slowlog_log_slower_than;    // 10000
+    int  slowlog_max_len;   //  128
+    int  hash_max_ziplist_entries;  // 512
+    int  hash_max_ziplist_value;    // 64
+    int  list_max_ziplist_entries;  // 512
+    int  list_max_ziplist_value;    // 64
+    int  set_max_intset_entries;    // 512
+    int  zset_max_ziplist_entries;  // 128
+    int  zset_max_ziplist_value;    // 64
+    int  activerehashing;           // yes
+    //        int  client_output_buffer_limit normal 0 0 0
+    //        int  client_output_buffer_limit slave 256mb 64mb 60
+    //        int  client_output_buffer_limit pubsub 32mb 8mb 60
+    int  hz;    //  10
+};
+
